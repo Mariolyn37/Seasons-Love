932b26ca9eab46c44b5f75104fc6e2c8034346ef
Mariolyn37 <114368891+Mariolyn37@users.noreply.github.com>
2022-09-25 19:11:32 -0500

    Add files via upload

---
+++ js/cap4.js
@@ -0,0 +1,180 @@
+var vineta3 = document.querySelector(".vineta3");
+var vineta5 = document.querySelector(".vineta5");
+var vineta6 = document.querySelector(".vineta6");
+var vineta9 = document.querySelector(".vineta9");
+var vineta10 = document.querySelector(".vineta10-1");
+var vineta11 = document.querySelector(".vineta10-2");
+var vineta13 = document.querySelector(".vineta13");
+var vineta15 = document.querySelector(".vineta15");
+var vineta16 = document.querySelector(".vineta16");
+var vineta18 = document.querySelector(".vineta18");
+var vineta19 = document.querySelector(".vineta19");
+var vineta20 = document.querySelector(".vineta20");
+
+vineta3.addEventListener("click", cambiov3);
+function cambiov3(){
+    if (vineta3==0) {
+        return
+    } else {
+        vineta3.setAttribute("src","./gift cap4/gift no infinitos/cap-4-animar_1.gif");  
+        vineta5.classList.add('sombra');
+        vineta3.style.animation = 'none';
+        vineta3 = 0;   
+    } 
+}
+
+vineta5.addEventListener("click", cambiov5);
+function cambiov5(){
+    if (vineta5==0) {
+        return
+    } else {
+        vineta5.setAttribute("src","./gift cap4/gift no infinitos/animar-grande1.gif");  
+        vineta6.classList.add('sombra');
+        vineta5.classList.remove('sombra');
+        vineta5 = 0;   
+    } 
+}
+
+vineta6.addEventListener("click", cambiov6);
+function cambiov6(){
+    if (vineta6==0) {
+        return
+    } else {
+        vineta6.setAttribute("src","./gift cap4/gift no infinitos/cap-4-animar_2.gif");  
+        vineta9.classList.add('sombra');
+        vineta6.classList.remove('sombra');
+        vineta6 = 0;   
+    } 
+}
+
+vineta9.addEventListener("click", cambiov9);
+function cambiov9(){
+    if (vineta9==0) {
+        return
+    } else {
+        vineta9.setAttribute("src","./gift cap4/gift no infinitos/cap-4-animar_3.gif");  
+        vineta10.classList.add('sombra');
+        vineta9.classList.remove('sombra');
+        vineta9 = 0;   
+    } 
+}
+
+vineta10.addEventListener("click", cambiov10);
+function cambiov10(){
+    if (vineta10==0) {
+        return
+    } else {
+        vineta10.setAttribute("src","./gift cap4/gift no infinitos/cap-4-animar_4.gif");  
+        vineta11.classList.add('sombra');
+        vineta10.classList.remove('sombra');
+        vineta10 = 0;   
+    } 
+}
+
+vineta11.addEventListener("click", cambiov11);
+function cambiov11(){
+    if (vineta11==0) {
+        return
+    } else {
+        vineta11.setAttribute("src","./gift cap4/gift no infinitos/cap-4-animar_5.gif");  
+        vineta13.classList.add('sombra');
+        vineta11.classList.remove('sombra');
+        vineta11 = 0;   
+    } 
+}
+
+vineta13.addEventListener("click", cambiov13);
+function cambiov13(){
+    if (vineta13==0) {
+        return
+    } else {
+        vineta13.setAttribute("src","./gift cap4/gift no infinitos/cap-4-animar_6.gif");  
+        vineta13.classList.add('sombra');
+        vineta13.classList.remove('sombra');
+        vineta13 = 0;   
+    } 
+}
+
+vineta13.addEventListener("click", cambiov13);
+function cambiov13(){
+    if (vineta13==0) {
+        return
+    } else {
+        vineta13.setAttribute("src","./gift cap4/gift no infinitos/cap-4-animar_6.gif");  
+        vineta15.classList.add('sombra');
+        vineta13.classList.remove('sombra');
+        vineta13 = 0;   
+    } 
+}
+
+vineta15.addEventListener("click", cambiov15);
+function cambiov15(){
+    if (vineta15==0) {
+        return
+    } else {
+        vineta15.setAttribute("src","./gift cap4/gift no infinitos/cap-4-animar_7.gif");  
+        vineta16.classList.add('sombra');
+        vineta15.classList.remove('sombra');
+        vineta15 = 0;   
+    } 
+}
+
+vineta16.addEventListener("click", cambiov16);
+function cambiov16(){
+    if (vineta16==0) {
+        return
+    } else {
+        vineta16.setAttribute("src","./gift cap4/gift no infinitos/cap-4-animar_8.gif");  
+        vineta18.classList.add('sombra');
+        vineta16.classList.remove('sombra');
+        vineta16 = 0;   
+    } 
+}
+
+vineta18.addEventListener("click", cambiov18);
+function cambiov18(){
+    if (vineta18==0) {
+        return
+    } else {
+        vineta18.setAttribute("src","./gift cap4/gift no infinitos/cap-4-animar_9.gif");  
+        vineta19.classList.add('sombra');
+        vineta18.classList.remove('sombra');
+        vineta18 = 0;   
+    } 
+}
+
+vineta19.addEventListener("click", cambiov19);
+function cambiov19(){
+    if (vineta19==0) {
+        return
+    } else {
+        vineta19.setAttribute("src","./gift cap4/gift no infinitos/cap-4-animar_10.gif");  
+        vineta20.classList.add('sombra');
+        vineta19.classList.remove('sombra');
+        vineta19 = 0;   
+    } 
+}
+
+vineta20.addEventListener("click", cambiov20);
+function cambiov20(){
+    if (vineta20==0) {
+        return
+    } else {
+        vineta20.setAttribute("src","./gift cap4/gift no infinitos/cap-4-animar_11.gif");  
+        vineta20.classList.add('sombra');
+        vineta20.classList.remove('sombra');
+        vineta20 = 0;   
+    } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
